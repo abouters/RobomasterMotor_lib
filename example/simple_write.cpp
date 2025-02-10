dc9aff1f129cd46af057526366bcbdc1c7ca2dc0
haru <huria0817@gmail.com>
2025-02-10 15:01:06 +0900

    Change  PIDgain

--- example/simple_write.cpp
+++ example/simple_write.cpp
@@ -5,8 +5,10 @@
 #include <IntervalTimer.h>
 #include <Metro.h>
 
-const PIDGain Rpmmotor = {3, 0.3, 0.5};
-const PIDGain Posmotor = {0.03, 0.0, 0.};
+const PIDGain RPM_M3508 = {3, 0.3, 0.5};
+const PIDGain POS_M3508 = {0.03, 0.0, 0.};
+const PIDGain RPM_M2006 = {2.5, 0.15, 5};
+const PIDGain POS_M2006 = {0.03, 0.0, 0.};
 const double control_cycle = 1.0; // 制御周期[ms]
 CanControl can1(1);
 RobomasterMotor motor(can1, control_cycle);
@@ -16,8 +18,8 @@ IntervalTimer calc_timer; // タイマー割り込み用インスタンス「
 void setup() {
   Serial.begin(115200);
   motor.setMotorType(1, M3508);           // 最初にモーターを指定する(M3508 or M2006)
-  motor.setRpmPIDgain(1, Rpmmotor);       // 速度制御用PIDゲインを設定する
-  motor.setPositionPIDgain(1, Posmotor);  // 位置制御用PIDゲインを設定する
+  motor.setRpmPIDgain(1, RPM_M3508);       // 速度制御用PIDゲインを設定する
+  motor.setPositionPIDgain(1, POS_M3508);  // 位置制御用PIDゲインを設定する
   calc_timer.begin(motor.interruptHandler, control_cycle*1000); // タイマー割り込みを開始する　[μs]なので制御周期を1000倍する
 }
 
